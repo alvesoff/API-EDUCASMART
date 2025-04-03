8f4101361076adcdcf33e8a127ea4b02b35a48b0
Gustavo Alves <103228297+alvesoff@users.noreply.github.com>
2025-04-03 16:27:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // Configuração do CORS para permitir requisições do frontend
 app.use(cors({
-  origin: ['https://frontend-one-kappa-88.vercel.app', process.env.CORS_ORIGIN],
+  origin: ['https://frontend-one-kappa-88.vercel.app', 'https://frontend-pys4p8cyp-alvesoffs-projects.vercel.app', process.env.CORS_ORIGIN],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 }));
 
 // Definir rotas
@@ -28,7 +28,7 @@ app.use('/api/resultados', require('./routes/resultados'));
 app.use('/api/questoes-pessoais', require('./routes/questoesPessoais'));
 
 // Rota padrão
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ msg: 'Bem-vindo à API do EducaSmart' });
 });
 
